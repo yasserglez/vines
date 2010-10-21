24545f7a189f77c01dc27392d9af043320f63f1e
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2010-10-21 13:00:18 -0400

    Not supporting optimMethod and corTestMethod specified as NULL.

--- R/fitVineML.R
+++ R/fitVineML.R
@@ -95,10 +95,6 @@ fitVineML <- function (type, data, trees = ncol(data) - 1, copulas = list(),
     corTestMethod = "kendall", corTestSigLevel = 0.1,
     gofCopulaIters = 1000, gofCopulaMethod = "mpl", gofCopulaSimul = "mult",
     optimMethod = "Nelder-Mead", optimControl = list()) {
-  
-  if (is.null(corTestMethod)) corTestMethod <- ""
-  if (is.null(optimMethod)) optimMethod <- ""
-
   # Function called by iterVine to select the bivariate copula that better 
   # fits the bivariate data from the given list of copulas.
   selectCopula <- function (vine, i, j, x, y) {
