7fdf95ea11bfcfa188312b0b93baf81d2451dadc
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2011-01-26 03:16:33 -0500

    Corrected identation in the test of the h-functions.

--- tests/hfunctions.R
+++ tests/hfunctions.R
@@ -12,27 +12,27 @@ V <- seq(from = 0.25, to = 0.75, length = N)
 XV <- merge(X, V)
 
 copulas <- c(
-    lapply(seq(from = -1, to = 1, length = P), 
-        function (rho) normalCopula(rho)),
-    apply(merge(seq(from = -1, to = 1, length = P),
-            seq(from = 1, to = 30, length = P)), 1,
-        function (p) tCopula(p[1], df = p[2], df.fixed = TRUE)),
-    lapply(seq(from = .Machine$double.eps, to = 3, length = P),
-        function (theta) claytonCopula(theta)),
-    lapply(seq(from = 1, to = 5, length = P),
-        function (theta) gumbelCopula(theta)),
-    list(indepCopula()))
+        lapply(seq(from = -1, to = 1, length = P), 
+                function (rho) normalCopula(rho)),
+        apply(merge(seq(from = -1, to = 1, length = P),
+                        seq(from = 1, to = 30, length = P)), 1,
+                function (p) tCopula(p[1], df = p[2], df.fixed = TRUE)),
+        lapply(seq(from = .Machine$double.eps, to = 3, length = P),
+                function (theta) claytonCopula(theta)),
+        lapply(seq(from = 1, to = 5, length = P),
+                function (theta) gumbelCopula(theta)),
+        list(indepCopula()))
 
 for (copula in copulas) {
-  # Validate the h-function.
-  u <- h(copula, XV[ , 1], XV[ , 2])
-  uu <- vines:::hCopula(copula, XV[ , 1], XV[ , 2])
-  stopifnot(isTRUE(all.equal(u, uu, T)))
-
-  # Validate the inverse of the h-function.
-  x <- hinverse(copula, u, XV[ , 2])
-  xx <- vines:::hinverseCopula(copula, u, XV[ , 2])
-  stopifnot(isTRUE(all.equal(x, xx, T)))
+    # Validate the h-function.
+    u <- h(copula, XV[ , 1], XV[ , 2])
+    uu <- vines:::hCopula(copula, XV[ , 1], XV[ , 2])
+    stopifnot(isTRUE(all.equal(u, uu, T)))
+    
+    # Validate the inverse of the h-function.
+    x <- hinverse(copula, u, XV[ , 2])
+    xx <- vines:::hinverseCopula(copula, u, XV[ , 2])
+    stopifnot(isTRUE(all.equal(x, xx, T)))
 }
 
 
@@ -41,41 +41,41 @@ V <- seq(from = 0, to = 1, length = N)
 XV <- merge(X, V)
 
 copulas <- c(
-    lapply(seq(from = -1, to = 1, length = P), 
-        function (rho) normalCopula(rho)),
-    apply(merge(seq(from = -1, to = 1, length = P),
-            seq(from = 1, to = 30, length = P)), 1,
-        function (p) tCopula(p[1], df = p[2], df.fixed = TRUE)),
-    lapply(seq(from = .Machine$double.eps, to = 100, length = P),
-        function (theta) claytonCopula(theta)),
-    lapply(seq(from = 1, to = 100, length = P),
-        function (theta) gumbelCopula(theta)),
-    list(indepCopula()))
+        lapply(seq(from = -1, to = 1, length = P), 
+                function (rho) normalCopula(rho)),
+        apply(merge(seq(from = -1, to = 1, length = P),
+                        seq(from = 1, to = 30, length = P)), 1,
+                function (p) tCopula(p[1], df = p[2], df.fixed = TRUE)),
+        lapply(seq(from = .Machine$double.eps, to = 100, length = P),
+                function (theta) claytonCopula(theta)),
+        lapply(seq(from = 1, to = 100, length = P),
+                function (theta) gumbelCopula(theta)),
+        list(indepCopula()))
 
 for (copula in copulas) {
-  # Check h(0, v) == 0.
-  u <- h(copula, rep(0, N), V)
-  stopifnot(isTRUE(all.equal(u, rep(0, N), T)))
-
-  # Check h(1, v) == 1.
-  u <- h(copula, rep(1, N), V)
-  stopifnot(isTRUE(all.equal(u, rep(1, N), T)))
-
-  # Check for finite return values in [0,1].
-  u <- h(copula, XV[ , 1], XV[ , 2])
-  stopifnot(all(is.finite(u)))
-  stopifnot(all(u >= 0 & u <= 1))
-
-  # Check for finite return values in [0,1].
-  x <- hinverse(copula, u, XV[ , 2])
-  stopifnot(all(is.finite(x)))
-  stopifnot(all(x >= 0 & x <= 1))
-
-  # Check hinverse(0, v) == 0.
-  x <- hinverse(copula, rep(0, N), V)
-  stopifnot(isTRUE(all.equal(x, rep(0, N), T)))
-
-  # Check hinverse(1, v) == 1.
-  x <- hinverse(copula, rep(1, N), V)
-  stopifnot(isTRUE(all.equal(x, rep(1, N), T)))
+    # Check h(0, v) == 0.
+    u <- h(copula, rep(0, N), V)
+    stopifnot(isTRUE(all.equal(u, rep(0, N), T)))
+    
+    # Check h(1, v) == 1.
+    u <- h(copula, rep(1, N), V)
+    stopifnot(isTRUE(all.equal(u, rep(1, N), T)))
+    
+    # Check for finite return values in [0,1].
+    u <- h(copula, XV[ , 1], XV[ , 2])
+    stopifnot(all(is.finite(u)))
+    stopifnot(all(u >= 0 & u <= 1))
+    
+    # Check for finite return values in [0,1].
+    x <- hinverse(copula, u, XV[ , 2])
+    stopifnot(all(is.finite(x)))
+    stopifnot(all(x >= 0 & x <= 1))
+    
+    # Check hinverse(0, v) == 0.
+    x <- hinverse(copula, rep(0, N), V)
+    stopifnot(isTRUE(all.equal(x, rep(0, N), T)))
+    
+    # Check hinverse(1, v) == 1.
+    x <- hinverse(copula, rep(1, N), V)
+    stopifnot(isTRUE(all.equal(x, rep(1, N), T)))
 }
