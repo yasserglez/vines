e0876d59835d033d9f6f1bca1fc578c60ccf67d4
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2011-02-05 15:37:39 -0500

    Setting the column names of the matrix returned by rvine.

--- R/rvine.R
+++ R/rvine.R
@@ -21,40 +21,43 @@ setGeneric("rvine",
 
 
 rCVine <- function (vine, n) {
-    # Algorithm 1 from Aas, K., Czado, C., Frigessi, A. & Bakken, H. 
-    # Pair-copula constructions of multiple dependence. Insurance 
+    # Algorithm 1 from Aas, K., Czado, C., Frigessi, A. & Bakken, H.
+    # Pair-copula constructions of multiple dependence. Insurance
     # Mathematics and Economics, 2009, Vol. 44, pp. 182-198.
-    
+
+    d <- vine@dimension
+
     if (vine@trees == 0) {
         # Vine without trees, Independent vine.
-        d <- vine@dimension
-        return(matrix(runif(n * d), n, d))
+        result <- matrix(runif(n * d), n, d)
+        colnames(result) <- dimnames(vine)
+        return(result)
     }
-    
-    d <- vine@dimension
+
     v <- matrix(NA, d, d)
     w <- matrix(runif(n * d), n, d)
     result <- matrix(NA, n, d)
-    
+    colnames(result) <- dimnames(vine)
+
     result[ , 1] <- w[ , 1]
-    
+
     for (s in seq(length = n)) { # Loop over samples.
         v[1, 1] <- result[s, 1]
         for (i in seq(from = 2, to = d)) { # Loop over the variables.
             v[i, 1] <- w[s, i]
-            for (k in seq(from = min(vine@trees, i - 1), to = 1)) {
+            for (k in seq(from = min(vine@trees, i-1), to = 1)) {
                 v[i, 1] <- hinverse(vine@copulas[[k, i-k]], v[i, 1], v[k, k])
             }
             result[s, i] <- v[i, 1]
             
             if (i == d) break
             
-            for (j in seq(from = 1, to = min(vine@trees, i - 1))) {
+            for (j in seq(from = 1, to = min(vine@trees, i-1))) {
                 v[i, j+1] <- h(vine@copulas[[j, i-j]], v[i, j], v[j, j])
             }
         }
     }
-    
+
     result
 }
 
@@ -66,16 +69,19 @@ rDVine <- function (vine, n) {
     # Pair-copula constructions of multiple dependence. Insurance 
     # Mathematics and Economics, 2009, Vol. 44, pp. 182-198.
     
+    d <- vine@dimension
+
     if (vine@trees == 0) {
         # Vine without trees, Independent vine.
-        d <- vine@dimension
-        return(matrix(runif(n * d), n, d))
+        result <- matrix(runif(n * d), n, d)
+        colnames(result) <- dimnames(vine)
+        return(result)
     }  
     
-    d <- vine@dimension
     w <- matrix(runif(n * d), n, d)
     v <- matrix(NA, d, max(2 * d - 4, d))
     result <- matrix(NA, n, d)
+    colnames(result) <- dimnames(vine)
     
     result[ , 1] <- w[ , 1]
     result[ , 2] <- hinverse(vine@copulas[[1, 1]], w[ , 2], w[ , 1])
@@ -90,19 +96,19 @@ rDVine <- function (vine, n) {
         for (i in seq(from = 3, to = d)) { # Loop over the variables.
             v[i, 1] <- w[s, i]
             if (vine@trees >= 2) {
-                for (k in seq(from = min(vine@trees, i - 1), to = 2)) {
+                for (k in seq(from = min(vine@trees, i-1), to = 2)) {
                     v[i, 1] <- hinverse(vine@copulas[[k, i-k]], v[i, 1], v[i-1, 2*k-2])
                 }
             }
             v[i, 1] <- hinverse(vine@copulas[[1, i-1]], v[i, 1], v[i-1, 1])
             result[s, i] <- v[i, 1]
-            
+
             if (i == d) break
             
             if (vine@trees >= 2) v[i, 2] <- h(vine@copulas[[1, i-1]], v[i-1, 1], v[i, 1])
             if (vine@trees >= 3) v[i, 3] <- h(vine@copulas[[1, i-1]], v[i, 1], v[i-1, 1])
             if (vine@trees >= 3 && i > 3) {
-                for (j in seq(from = 2, to = min(vine@trees - 1, i - 2))) {
+                for (j in seq(from = 2, to = min(vine@trees-1, i-2))) {
                     v[i, 2*j] <- h(vine@copulas[[j, i-j]], v[i-1, 2*j-2], v[i, 2*j-1])
                     v[i, 2*j+1] <- h(vine@copulas[[j, i-j]], v[i, 2*j-1], v[i-1, 2*j-2])                       
                 }
@@ -110,7 +116,7 @@ rDVine <- function (vine, n) {
             if (vine@trees >= i) v[i, 2*i-2] <- h(vine@copulas[[i-1, 1]], v[i-1, 2*i-4], v[i, 2*i-3])
         }
     }
-    
+
     result
 }
 
