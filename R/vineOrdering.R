bc234af78d89ba49db4c81ee25955328f6bb0d74
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2011-02-15 11:38:20 -0500

    Improving the greedy method to select the order of the variables.

--- R/vineOrdering.R
+++ R/vineOrdering.R
@@ -16,73 +16,45 @@
 # this program. If not, see <http://www.gnu.org/licenses/>.
 
 vineOrderingGreedy <- function (type, data, according = "kendall") {
+    n <- ncol(data)
+
     if (according %in% c("pearson", "kendall", "spearman")) {
         # Calculate the value of the given measure of association between 
         # each pair of variables and couple the variables with the largest 
-        # modular values.
-        values <- abs(cor(data, method = according))
-        diag(values) <- -Inf
+        # absolute values.
+        weights <- 1 - abs(cor(data, method = according))
     } else if (according %in% c("df")) {
-        # Fit bivariate t copulas to each pair of variables and couple the 
-        # variables with the smaller values of the degrees of freedom.
-        values <- matrix(-Inf, ncol(data), ncol(data))
-        for (i in seq(length = ncol(data))) {
-            for (j in seq(length = max(i - 1, 0))) {
-                x <- data[ , i]
+        # Fit bivariate t copulas to each pair of variables and couple
+        # the variables with the smaller degrees of freedom.
+        weights <- matrix(0, n, n)
+        for (currentRoot in seq(length = n)) {
+            for (j in seq(length = max(currentRoot - 1, 0))) {
+                x <- data[ , currentRoot]
                 y <- data[ , j]
                 copula <- tCopula(0)
                 rho <- calibKendallsTau(copula, cor(x, y, method = "kendall"))
                 L <- function (df) loglikCopula(c(rho, df), cbind(x, y), copula)
-                df <- optim(copula@parameters[2], L, method = "BFGS",
-                        control = list(fnscale = -1))$par
-                values[i, j] <- -df
-                values[j, i] <- -df
+                df <- optimize(L, c(1, 30), maximum = TRUE)$maximum
+                weights[currentRoot, j] <- df
+                weights[j, currentRoot] <- df
             }
         }
     } else {
         stop("invalid value ", dQuote(according), " for the according argument")
     }
 
-    # Try to couple the pairs with the maximum value in the values matrix, 
-    
-    n <- ncol(data)
-    k <- which.max(values)
-    i <- row(values)[k]
-    j <- col(values)[k]
-    values[i, j] <- -Inf
-    values[j, i] <- -Inf
-    
+    # Couple the pairs with the minimum values in the values matrix.
+
     if (identical(type, "DVine")) {
-        ordering <- c(i, j)
-        while (length(ordering) < n) {
-            i <- ordering[1]
-            j <- ordering[length(ordering)]
-            if (max(values[i, ]) >= max(values[j, ])) {
-                ii <- which.max(values[i, ])
-                for (k in ordering) {
-                    values[k, ii] <- -Inf
-                    values[ii, k] <- -Inf
-                }
-                ordering <- c(ii, ordering)
-            } else {
-                jj <- which.max(values[j, ])
-                for (k in ordering) {
-                    values[k, jj] <- -Inf
-                    values[jj, k] <- -Inf
-                }
-                ordering <- c(ordering, jj)
-            }
-        }
+        tsp <- insert_dummy(as.TSP(weights), label = "dummy")
+        tour <- solve_TSP(tsp, method = "cheapest_insertion")
+        ordering <- cut_tour(tour, "dummy")
+        names(ordering) <- NULL
     } else if (identical(type, "CVine")) {
-        if (max(values[i, ]) >= max(values[j, ])) {
-            ii <- which.max(values[i, ])
-            ordering <- c(i, ii, j, seq(to = n)[c(-i, -ii, -j)])
-        } else {
-            jj <- which.max(values[j, ])
-            ordering <- c(j, jj, i, seq(to = n)[c(-j, -jj, -i)])
-        }
+        root <- which.min(colSums(weights))
+        ordering <- c(root, seq(to = n)[-root])
     }
-    
+
     ordering
 }
 
