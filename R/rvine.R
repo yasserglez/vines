96365c85e420e575a19d780f49240bd391928750
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2011-05-11 03:02:25 -0400

    Changes for the 1.0.0 version.

--- R/rvine.R
+++ R/rvine.R
@@ -1,18 +1,18 @@
-# vines: R package for multivariate dependence modeling with vines
-# Copyright (C) 2010-2011 Yasser González-Fernández
-# Copyright (C) 2010-2011 Marta Soto
+# vines: Multivariate Dependence Modeling with Vines
+# Copyright (C) 2010, 2011 Yasser González-Fernández
+# Copyright (C) 2010, 2011 Marta Soto
 #
 # This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software 
-# Foundation, either version 3 of the License, or (at your option) any later 
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
 # version.
 #
-# This program is distributed in the hope that it will be useful, but WITHOUT 
+# This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
 # details.
 #
-# You should have received a copy of the GNU General Public License along with 
+# You should have received a copy of the GNU General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
 
 setGeneric("rvine",
@@ -28,7 +28,7 @@ rCVine <- function (vine, n) {
     d <- vine@dimension
 
     if (vine@trees == 0) {
-        # Vine without trees, Independent vine.
+        # Independent vine.
         result <- matrix(runif(n * d), n, d)
         colnames(result) <- dimnames(vine)
         return(result)
@@ -72,7 +72,7 @@ rDVine <- function (vine, n) {
     d <- vine@dimension
 
     if (vine@trees == 0) {
-        # Vine without trees, Independent vine.
+        # Independent vine.
         result <- matrix(runif(n * d), n, d)
         colnames(result) <- dimnames(vine)
         return(result)
