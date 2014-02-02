eead54bcf5150672166ab29f8623f7b200b2ef31
Yasser Gonzalez Fernandez <ygonzalezfernandez@gmail.com>
2014-02-01 23:50:12 -0500

    Naming the 3rd all.equal argument tolerance (not in the generic).

--- tests/hfunctions.R
+++ tests/hfunctions.R
@@ -1,74 +1,74 @@
 library("vines")
 
-N <- 7 # Number of values of each variable.
-P <- 5 # Number of values of each parameter.
-T <- 0.01 # Tolerance checking for equality.
+n <- 7 # Number of values of each variable.
+np <- 5 # Number of values of each parameter.
+tol <- 0.01 # Differences smaller than tol are not considered.
 
-X <- seq(from = 0.25, to = 0.75, length = N)
-V <- seq(from = 0.25, to = 0.75, length = N)
+X <- seq(from = 0.25, to = 0.75, length = n)
+V <- seq(from = 0.25, to = 0.75, length = n)
 XV <- merge(X, V)
 
 copulas <- c(
-        lapply(seq(from = -1, to = 1, length = P), 
-                function (rho) normalCopula(rho)),
-        apply(merge(seq(from = -1, to = 1, length = P),
-                        seq(from = 1, to = 30, length = P)), 1,
-                function (p) tCopula(p[1], df = as.integer(p[2]), df.fixed = TRUE)),
-        lapply(seq(from = .Machine$double.eps^0.5, to = 3, length = P),
-                function (theta) claytonCopula(theta)),
-        lapply(seq(from = 1, to = 5, length = P),
-                function (theta) gumbelCopula(theta)),
-        lapply(seq(from = -1, to = 1, length = P), 
-                function (theta) fgmCopula(theta)),
-        lapply(seq(from = 1, to = 5, length = P),
-                function (theta) galambosCopula(theta)),       
-        lapply(seq(from = -10, to = 10, length = P),
-                function (theta) frankCopula(theta)),
-        list(indepCopula()))
+    lapply(seq(from = -1, to = 1, length = np), 
+        function (rho) normalCopula(rho)),
+    apply(merge(seq(from = -1, to = 1, length = np),
+            seq(from = 1, to = 30, length = np)), 1,
+        function (p) tCopula(p[1], df = as.integer(p[2]), df.fixed = TRUE)),
+    lapply(seq(from = .Machine$double.eps^0.5, to = 3, length = np),
+        function (theta) claytonCopula(theta)),
+    lapply(seq(from = 1, to = 5, length = np),
+        function (theta) gumbelCopula(theta)),
+    lapply(seq(from = -1, to = 1, length = np), 
+        function (theta) fgmCopula(theta)),
+    lapply(seq(from = 1, to = 5, length = np),
+        function (theta) galambosCopula(theta)),       
+    lapply(seq(from = -10, to = 10, length = np),
+        function (theta) frankCopula(theta)),
+    list(indepCopula()))
 
 for (copula in copulas) {
     # Validate the h-function.
     u <- h(copula, XV[ , 1], XV[ , 2])
     uu <- vines:::hCopula(copula, XV[ , 1], XV[ , 2])
-    stopifnot(isTRUE(all.equal(u, uu, T)))
-    
+    stopifnot(isTRUE(all.equal(u, uu, tolerance = tol)))
+
     # Validate the inverse of the h-function.
     x <- hinverse(copula, u, XV[ , 2])
     xx <- vines:::hinverseCopula(copula, u, XV[ , 2])
-    stopifnot(isTRUE(all.equal(x, xx, T)))
+    stopifnot(isTRUE(all.equal(x, xx, tolerance = tol)))
 }
 
 
-X <- seq(from = 0, to = 1, length = N)
-V <- seq(from = 0, to = 1, length = N)
+X <- seq(from = 0, to = 1, length = n)
+V <- seq(from = 0, to = 1, length = n)
 XV <- merge(X, V)
 
 copulas <- c(
-        lapply(seq(from = -1, to = 1, length = P), 
-                function (rho) normalCopula(rho)),
-        apply(merge(seq(from = -1, to = 1, length = P),
-                        seq(from = 1, to = 30, length = P)), 1,
-                function (p) tCopula(p[1], df = as.integer(p[2]), df.fixed = TRUE)),
-        lapply(seq(from = .Machine$double.eps^0.5, to = 100, length = P),
-                function (theta) claytonCopula(theta)),
-        lapply(seq(from = 1, to = 100, length = P),
-                function (theta) gumbelCopula(theta)),
-        lapply(seq(from = -1, to = 1, length = P),
-                function (theta) fgmCopula(theta)),
-        lapply(seq(from = 1, to = 25, length = P),
-                function (theta) galambosCopula(theta)),
-        lapply(seq(from = -100, to = 100, length = P),
-                function (theta) frankCopula(theta)),       
-        list(indepCopula()))
+    lapply(seq(from = -1, to = 1, length = np), 
+        function (rho) normalCopula(rho)),
+    apply(merge(seq(from = -1, to = 1, length = np),
+            seq(from = 1, to = 30, length = np)), 1,
+        function (p) tCopula(p[1], df = as.integer(p[2]), df.fixed = TRUE)),
+    lapply(seq(from = .Machine$double.eps^0.5, to = 100, length = np),
+        function (theta) claytonCopula(theta)),
+    lapply(seq(from = 1, to = 100, length = np),
+        function (theta) gumbelCopula(theta)),
+    lapply(seq(from = -1, to = 1, length = np),
+        function (theta) fgmCopula(theta)),
+    lapply(seq(from = 1, to = 25, length = np),
+        function (theta) galambosCopula(theta)),
+    lapply(seq(from = -100, to = 100, length = np),
+        function (theta) frankCopula(theta)),       
+    list(indepCopula()))
 
 for (copula in copulas) {
     # Check h(0, v) == 0.
-    u <- h(copula, rep(0, N), V)
-    stopifnot(isTRUE(all.equal(u, rep(0, N), T)))
+    u <- h(copula, rep(0, n), V)
+    stopifnot(isTRUE(all.equal(u, rep(0, n), tolerance = tol)))
 
     # Check h(1, v) == 1.
-    u <- h(copula, rep(1, N), V)
-    stopifnot(isTRUE(all.equal(u, rep(1, N), T)))
+    u <- h(copula, rep(1, n), V)
+    stopifnot(isTRUE(all.equal(u, rep(1, n), tolerance = tol)))
 
     # Check for finite return values in [0,1].
     u <- h(copula, XV[ , 1], XV[ , 2])
@@ -81,10 +81,10 @@ for (copula in copulas) {
     stopifnot(all(x >= 0 & x <= 1))
 
     # Check hinverse(0, v) == 0.
-    x <- hinverse(copula, rep(0, N), V)
-    stopifnot(isTRUE(all.equal(x, rep(0, N), T)))
+    x <- hinverse(copula, rep(0, n), V)
+    stopifnot(isTRUE(all.equal(x, rep(0, n), tolerance = tol)))
 
     # Check hinverse(1, v) == 1.
-    x <- hinverse(copula, rep(1, N), V)
-    stopifnot(isTRUE(all.equal(x, rep(1, N), T)))
+    x <- hinverse(copula, rep(1, n), V)
+    stopifnot(isTRUE(all.equal(x, rep(1, n), tolerance = tol)))
 }
