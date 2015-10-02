5c3ffebfbf93184979a86d04e6a09521cbcaa56f
Yasser Gonzalez <yasserglez@gmail.com>
2015-10-02 13:28:49 -0400

    Add test_that wrapper to simplify the h/hinverse test code

--- tests/testthat/test-h.R
+++ tests/testthat/test-h.R
@@ -1,33 +1,25 @@
 context("h")
 
-test_that("h() returns finite values in [0,1]", {
-    for (copula in copulas) {
-        u <- h(copula, XV[ , 1], XV[ , 2])
-        expect_true(all(is.finite(u)))
-        expect_true(all(u >= 0 & u <= 1))
-    }
+test_that_for_each_copula("h() in [0,1]", {
+    u <- h(copula, XV[ , 1], XV[ , 2])
+    expect_true(all(is.finite(u)))
+    expect_true(all(u >= 0 & u <= 1))
 })
 
-test_that("h() computes the correct values", {
+test_that_for_each_copula("h() is correct", {
     XV <- subset(XV, X >= 0.25 & X <= 0.75 & V >= 0.25 & V <= 0.75)
-    for (copula in copulas) {
-        u <- h(copula, XV[ , 1], XV[ , 2])
-        uu <- vines:::hCopula(copula, XV[ , 1], XV[ , 2],
-                              eps = .Machine$double.eps^0.5)
-        expect_equal(u, uu, tolerance = tol)
-    }
+    u <- h(copula, XV[ , 1], XV[ , 2])
+    uu <- vines:::hCopula(copula, XV[ , 1], XV[ , 2],
+                          eps = .Machine$double.eps^0.5)
+    expect_equal(u, uu, tolerance = tol)
 })
 
-test_that("h(0, v) == 0", {
-    for (copula in copulas) {
-        u <- h(copula, rep(0, n), V)
-        expect_equal(u, rep(0, n), tolerance = tol)
-    }
+test_that_for_each_copula("h(0, v) == 0", {
+    u <- h(copula, rep(0, n), V)
+    expect_equal(u, rep(0, n), tolerance = tol)
 })
 
-test_that("h(1, v) == 1", {
-    for (copula in copulas) {
-        u <- h(copula, rep(1, n), V)
-        expect_equal(u, rep(1, n), tolerance = tol)
-    }
+test_that_for_each_copula("h(1, v) == 1", {
+    u <- h(copula, rep(1, n), V)
+    expect_equal(u, rep(1, n), tolerance = tol)
 })
--- tests/testthat/test-hinverse.R
+++ tests/testthat/test-hinverse.R
@@ -1,33 +1,25 @@
 context("hinverse")
 
-test_that("hinverse() returns finite values in [0,1]", {
-    for (copula in copulas) {
-        x <- hinverse(copula, XV[ , 1], XV[ , 2])
-        expect_true(all(is.finite(x)))
-        expect_true(all(x >= 0 & x <= 1))
-    }
+test_that_for_each_copula("hinverse() in [0,1]", {
+    x <- hinverse(copula, XV[ , 1], XV[ , 2])
+    expect_true(all(is.finite(x)))
+    expect_true(all(x >= 0 & x <= 1))
 })
 
-test_that("hinverse() computes the correct values", {
+test_that_for_each_copula("hinverse() is correct", {
     XV <- subset(XV, X >= 0.25 & X <= 0.75 & V >= 0.25 & V <= 0.75)
-    for (copula in copulas) {
-        x <- hinverse(copula, XV[ , 1], XV[ , 2])
-        xx <- vines:::hinverseCopula(copula, XV[ , 1], XV[ , 2],
-                                     eps = .Machine$double.eps^0.5)
-        expect_equal(x, xx, tolerance = tol)
-    }
+    x <- hinverse(copula, XV[ , 1], XV[ , 2])
+    xx <- vines:::hinverseCopula(copula, XV[ , 1], XV[ , 2],
+                                 eps = .Machine$double.eps^0.5)
+    expect_equal(x, xx, tolerance = tol)
 })
 
-test_that("hinverse(0, v) == 0", {
-    for (copula in copulas) {
-        x <- hinverse(copula, rep(0, n), V)
-        expect_equal(x, rep(0, n), tolerance = tol)
-    }
+test_that_for_each_copula("hinverse(0, v) == 0", {
+    x <- hinverse(copula, rep(0, n), V)
+    expect_equal(x, rep(0, n), tolerance = tol)
 })
 
-test_that("hinverse(1, v) == 1", {
-    for (copula in copulas) {
-        x <- hinverse(copula, rep(1, n), V)
-        expect_equal(x, rep(1, n), tolerance = tol)
-    }
+test_that_for_each_copula("hinverse(1, v) == 1", {
+    x <- hinverse(copula, rep(1, n), V)
+    expect_equal(x, rep(1, n), tolerance = tol)
 })
